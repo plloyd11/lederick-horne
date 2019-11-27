adf0934f1abe5482945938c6379b81ac47a0a286
plloyd11 <plloyd11@gmail.com>
2019-11-27 14:11:15 -0500

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+    el: '#test',
+    data: {
+        baller: 'BALLER'
+    }
+})
+
+new Vue({
+    el: '#nav',
+    data: function() {
+      return {
+        isOpen: false
+      }
+    },
+    methods: {
+      open() {
+        this.isOpen = true
+        this.$nextTick(() => {
+          this.$refs.closeButton.focus()
+        })
+      },
+      close() {
+        this.isOpen = false
+        this.$nextTick(() => {
+          this.$refs.openButton.focus()
+        })
+      }
+    },
+    watch: {
+      isOpen: {
+        immediate: true,
+        handler(isOpen) {
+          if (isOpen) {
+            document.body.style.setProperty('overflow', 'hidden')
+          } else {
+            document.body.style.removeProperty('overflow')
+          }
+        }
+      }
+    }
+  })
